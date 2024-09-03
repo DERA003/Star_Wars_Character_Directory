8872bf54d618d60350f15bc5328e49daf406709a
Chidera Mabel Umejuru <deraumejuru@gmail.com>
2024-09-03 23:21:52 +0100

    added images

--- starwars.js
+++ starwars.js
@@ -1,8 +1,15 @@
-function getRandomImages(){
-const pexelKey = ""
-}
-
-
+const images = [
+    "https://vignette.wikia.nocookie.net/star-wars-extended-universe/images/3/3d/LukeSkywalker.png/revision/latest?cb=20200125105040",
+    "https://th.bing.com/th/id/R.490f515c02460a5eb59163ffb26e1eff?rik=Fzr0NwBJSQLjPA&pid=ImgRaw&r=0",
+    "https://th.bing.com/th/id/OIP.nh5a2YG-H0h1p-YQr2ILRQHaLz?w=124&h=197&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+    "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0bc3f0bd-bb82-4799-bb3f-fb01aa4f6c4d/dbwsppz-1f4af0ad-beef-4f4e-8efe-d4e39207da42.jpg/v1/fit/w_750,h_1124,q_70,strp/darth_vader_by_adenry_dbwsppz-375w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MjM5NyIsInBhdGgiOiJcL2ZcLzBiYzNmMGJkLWJiODItNDc5OS1iYjNmLWZiMDFhYTRmNmM0ZFwvZGJ3c3Bwei0xZjRhZjBhZC1iZWVmLTRmNGUtOGVmZS1kNGUzOTIwN2RhNDIuanBnIiwid2lkdGgiOiI8PTE2MDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.xhf4bLLEQI1g41lBG2zLCtCz5zK-PV589QUO1lOnSN4",
+    "https://th.bing.com/th/id/R.b83e256e348c5952d16c317a38be2e2d?rik=pMdZcSFyeQc7mg&riu=http%3a%2f%2fimages5.fanpop.com%2fimage%2fphotos%2f31800000%2fprincess-leia-princess-leia-organa-solo-skywalker-31897480-2026-2560.jpg&ehk=fqnc8%2b3yr3C1z92iYW3wm4InDqbfukJSF860JuFHN7I%3d&risl=&pid=ImgRaw&r=0",
+    "https://th.bing.com/th/id/R.842c52ad92d955afbf06af189fda6b02?rik=86OUaXkOclQ%2bLw&riu=http%3a%2f%2fimg3.wikia.nocookie.net%2f__cb20080313203444%2fstarwars%2fimages%2f3%2f33%2fYoung_owen.jpg&ehk=1mkaFC9qNyF1s%2fHTuPRWX6oWhK2XJF7Pn3zG6PiNEy0%3d&risl=&pid=ImgRaw&r=0",
+    "https://th.bing.com/th/id/OIP.zwgYDXxcwsHLo1eVdxZ_KQAAAA?rs=1&pid=ImgDetMain",
+    "https://mlpnk72yciwc.i.optimole.com/cqhiHLc.IIZS~2ef73/w:auto/h:auto/q:75/https://bleedingcool.com/wp-content/uploads/2023/04/STAR-WARS-THE-BLACK-SERIES-R5-D4-4.jpg",
+    "https://th.bing.com/th/id/OIP.dTKMXyMZm3lmJ4TErXKgmwAAAA?rs=1&pid=ImgDetMain",
+    "https://th.bing.com/th/id/OIP.kzui0EE1XcarsabzUHratQHaHa?w=163&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7"
+]
 
 
 function starWarsCharacters() {
@@ -17,10 +24,10 @@ function starWarsCharacters() {
         // created a container in the Html character list:this is where the characters would be displayed
         const characterList = document.getElementById("character_list")
         // to get individual characters i looped through the data results
-        data.results.forEach(character => {
+        data.results.forEach((character, index) => {
           const  characterHTML = `
               <div class="character">
-                 <img src="${character.gender === 'female' ? 'https://csihospitalbangalore.org/sites/default/files/media/image/female_1.png' : "https://th.bing.com/th/id/R.8027ff280865c2c396ef1e6080f31028?rik=soF7YMi%2ftlSJmQ&pid=ImgRaw&r=0" }"alt="${character.name}"/>
+                 <img src="${images[index]}"alt="${character.name}"/>
                  <h2>${character.name}</h2>
                  <button>See More...</button>
               </div>
